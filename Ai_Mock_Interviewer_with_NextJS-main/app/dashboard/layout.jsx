1190264f6e93466d962459734079f604db7de947
kadamrudraksha <150592791+kadamrudraksha@users.noreply.github.com>
2025-09-14 20:25:40 +0530

    Update layout.jsx

--- Ai_Mock_Interviewer_with_NextJS-main/app/dashboard/layout.jsx
+++ Ai_Mock_Interviewer_with_NextJS-main/app/dashboard/layout.jsx
@@ -1,23 +1,10 @@
-"use client";
-import React, { createContext, useState } from "react";
+import React from "react";
 import { Header } from "./_components/Header";
-
-// 1. Create Context
-export const WebCamContext = createContext({
-  webCamEnabled: false,
-  setWebCamEnabled: () => {},
-});
-
 function DashboardLayout({ children }) {
-  // 2. State for webcam
-  const [webCamEnabled, setWebCamEnabled] = useState(false);
-
   return (
-    <WebCamContext.Provider value={{ webCamEnabled, setWebCamEnabled }}>
+    <div>
       <Header />
-      <div className="mx-5 md:mx-20 lg:mx-36">{children}</div>
-    </WebCamContext.Provider>
+      <div className="mx-5 md:mx-20 lg:,mx:36">{children}</div>
+    </div>
   );
 }
-
-export default DashboardLayout;
